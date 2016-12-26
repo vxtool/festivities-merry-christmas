77411a94fe4208f08ca76ad083cc87201bbfb3a4
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-12-25 23:56:47 -0200

    [add] on click

--- 2016/scripts.js
+++ 2016/scripts.js
@@ -18,3 +18,33 @@ $box.mouseenter(function(){
 
   tlJumpPresent.play();
 });
+
+$box.click(function(event){
+  event.preventDefault();
+
+  var $thisBox = this;
+  var $PresentBoxRibbon = $(this).find('.PresentBoxRibbon');
+  var $PresentBoxTop = $(this).find('.boxTop');
+  var $PresentBoxTopShadow = $(this).find('.boxTopShadow');
+  var $PresentRibbonSide = $(this).find('.ribbonSide');
+  var $socialLinkP = $(this).find('.socialLinkP');
+
+  tlOpenPresent = new TimelineMax({paused: true});
+  tlOpenPresent
+  .to($PresentBoxRibbon, 0.4, {yPercent: 252, ease:Power4.easeInOut})
+  .to($PresentBoxTop, 0.4, {yPercent: -80, ease:Power4.easeOut}, "0")
+  .to($PresentBoxTopShadow, 0.2, {autoAlpha: 0}, "0")
+  .to($PresentRibbonSide, 0.4, {scaleY: 0.3, transformOrigin:"bottom center", onComplete: stopHover, onCompleteParams: [$thisBox]}, "0.2")
+
+  .to($PresentBoxTop, 0.4, {rotation: -90, transformOrigin:"left center", ease:Power4.easeInOut}, "0")
+  .to($PresentBoxTop, 0.3, {yPercent: 400, transformOrigin:"left center", ease:Bounce.easeOut}, "0.4")
+  .to($PresentBoxTop, 0.4, {rotation: -180, transformOrigin:"left center", ease:Power4.easeIn}, "0.7")
+  .to($socialLinkP, 0.6, {scale: 1.4, yPercent: -260, transformOrigin:"top center", ease:Power4.easeInOut}, "-=0.4");
+
+  tlOpenPresent.play();
+
+  function stopHover(element){
+    $(element).unbind('mouseenter click');
+    $(element).css('cursor: default');
+  }
+});
