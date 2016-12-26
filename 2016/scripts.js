aa8ae33db8af7ef2e3864d33a91d013ce99f9cd8
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-12-25 23:54:35 -0200

    [add] mouse enter

--- 2016/scripts.js
+++ 2016/scripts.js
@@ -3,3 +3,18 @@ var $box = $('.box');
 var $PresentB = $('#PresentB');
 var $sIcons = $('.sIcon');
 var $socialLinkP = $('.socialLinkP');
+
+tlJumpPresent = new TimelineMax({paused: true});
+
+$box.mouseenter(function(){
+  var $PresentBox = $(this).find('.PresentBox');
+  var $PresentBoxShadow = $(this).find('.PresentBoxShadow');
+
+  tlJumpPresent
+  .to($PresentBox, 0.5, {scaleX: 0.8, transformOrigin:"bottom center", y: -20, ease:Power4.easeInOut})
+  .to($PresentBox, 0.2, {scaleX: 1, transformOrigin:"bottom center", y: 0, ease:Bounce.easeOut})
+  .to($PresentBoxShadow, 0.5, {scaleX: 0.8, transformOrigin:"bottom center", ease:Power4.easeInOut}, "0")
+  .to($PresentBoxShadow, 0.2, {scaleX: 1, transformOrigin:"bottom center", ease:Bounce.easeOut}, "0.5");
+
+  tlJumpPresent.play();
+});
